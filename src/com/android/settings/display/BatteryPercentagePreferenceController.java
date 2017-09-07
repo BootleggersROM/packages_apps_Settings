412411594676bd1e8c1c034974fe5d1ca1a9a2e8
ezio84 <brabus84@gmail.com>
2017-09-07 18:31:27 +0200

    Enable battery percentage in statusbar by default [2/2]

--- src/com/android/settings/display/BatteryPercentagePreferenceController.java
+++ src/com/android/settings/display/BatteryPercentagePreferenceController.java
@@ -50,7 +50,7 @@ public class BatteryPercentagePreferenceController extends PreferenceController
     @Override
     public void updateState(Preference preference) {
         int setting = Settings.System.getInt(mContext.getContentResolver(),
-                SHOW_BATTERY_PERCENT, 0);
+                SHOW_BATTERY_PERCENT, 1);
 
         ((SwitchPreference) preference).setChecked(setting == 1);
     }
