2f3bf5ceddf181c9ef40965a8437e12a9301a206
maxwen <max.weninger@gmail.com>
2017-09-22 16:42:48 -0400

    Integrate BootlegDumpster into Settings
    Add the HOME in chinese to make people question about it, but it's all related to make them feel like home

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -190,5 +190,6 @@ public class Settings extends SettingsActivity {
     public static class StorageDashboardActivity extends SettingsActivity {}
     public static class UserAndAccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
+    public static class BootlegDumpsterActivity extends SettingsActivity {}
 
 }
