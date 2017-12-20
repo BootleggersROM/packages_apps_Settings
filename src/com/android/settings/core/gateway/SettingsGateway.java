nitrogen-project/android_packages_apps_Settings

add activity to launch headsup settings fragment
Change-Id: I6587a1bf7e132e10759b5a9d858ddfd3ed866b22

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -127,6 +127,7 @@ import com.android.settings.wifi.WifiInfo;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import com.nitrogen.settings.fragments.HeadsUpSettings;
 import com.nitrogen.settings.NitrogenSettings;
 
 public class SettingsGateway {
@@ -247,6 +248,7 @@ public class SettingsGateway {
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
             NitrogenSettings.class.getName(),
+            HeadsUpSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

BootleggersROM/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -128,6 +128,7 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.bootleggers.dumpster.MainDump;
+import com.bootleggers.dumpster.external.HeadsUpSettings;
 
 public class SettingsGateway {
 
@@ -247,6 +248,7 @@ public class SettingsGateway {
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
             MainDump.class.getName(),
+            HeadsUpSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {