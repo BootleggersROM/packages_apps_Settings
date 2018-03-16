8dc184e22ac888922e4eb524071e82ebee326185
ElDainosor <eldainosor@gmail.com>
2018-03-16 04:51:13 +0000

    Revert "Set System Animations to .7 by default (2 of 2)"
    This reverts commit 5a9ac14f8309c782957d543604afb622903c2d05.

--- src/com/android/settings/development/DevelopmentSettings.java
+++ src/com/android/settings/development/DevelopmentSettings.java
@@ -2279,7 +2279,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     private void writeAnimationScaleOption(int which, AnimationScalePreference pref,
             Object newValue) {
         try {
-            float scale = newValue != null ? Float.parseFloat(newValue.toString()) : 0.7f;
+            float scale = newValue != null ? Float.parseFloat(newValue.toString()) : 1;
             mWindowManager.setAnimationScale(which, scale);
             updateAnimationScaleValue(which, pref);
         } catch (RemoteException e) {
