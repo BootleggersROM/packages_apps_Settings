6bb4dceb5028759f30f99a79714900eb56e18a3d
ElDainosor <eldainosor@gmail.com>
2018-08-21 23:01:22 +0000

    Revert "Settings: use CUSTOM_FINGERPRINT for search index check"
    This reverts commit 9ead9fc981a98d21e1573a28c3f0cf15dbdb8f9d.

--- src/com/android/settings/slices/SlicesDatabaseHelper.java
+++ src/com/android/settings/slices/SlicesDatabaseHelper.java
@@ -217,6 +217,6 @@ public class SlicesDatabaseHelper extends SQLiteOpenHelper {
 
     @VisibleForTesting
     String getBuildTag() {
-        return Build.CUSTOM_FINGERPRINT;
+        return Build.FINGERPRINT;
     }
 }
