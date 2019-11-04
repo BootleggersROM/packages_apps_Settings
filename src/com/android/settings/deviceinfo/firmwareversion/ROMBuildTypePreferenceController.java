1483aba283e2042ee6f36fa8501140ecb293f117
El Dainosor <eldainosor@gmail.com>
2019-11-04 01:44:49 +0000

    settings: Show more info on Firmware Info
    Android 10 decided to show all the device infor like ROM security patch and all that in their own section.
    Let's clutter it up with our special info like Date, Release type, and our Musical Codename
    
    Took some hints over the following files/commits:
    	https://android.googlesource.com/platform/frameworks/base/+/android-9.0.0_r3/packages/SettingsLib/src/com/android/settingslib/DeviceInfoUtils.java#155
    	https://github.com/BootleggersROM/packages_apps_Settings/commit/3793b05ab909a15180492eb42910398255ab09c2
    	https://android.googlesource.com/platform/packages/apps/Settings/+/android-9.0.0_r3/src/com/android/settings/deviceinfo/firmwareversion/SecurityPatchLevelDialogController.java
    And for 10, we took it from the following change:
    	https://github.com/LineageOS/android_packages_apps_Settings/commit/e66ea91fa635368de520bb4ca3c537c4a35f215e
    	https://github.com/BootleggersROM/packages_apps_Settings/commit/4851b75ddc98424961c001953f279bd7ea2f56cf
    
    NOTE: GPlay version removed, sorted everything and added our about info on the same fragment
    Change-Id: Ib41ca133664907882e312464241e02607052aa53

---
+++ src/com/android/settings/deviceinfo/firmwareversion/ROMBuildTypePreferenceController.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class ROMBuildTypePreferenceController  extends BasePreferenceController {
+
+    private static final String TAG = "ROMBuildTypePreferenceController";
+    private static final String BOOTLEGGERS_RELEASETYPE = "ro.bootleggers.releasetype";
+
+    public ROMBuildTypePreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String buildType = SystemProperties.get(BOOTLEGGERS_RELEASETYPE);
+        switch (buildType) {
+            case "Shishufied":
+                return mContext.getString(R.string.bootleggers_releasetype_skeleton, buildType, mContext.getString(R.string.bootleg_type_official));
+
+            case "Unshishufied":
+                return mContext.getString(R.string.bootleggers_releasetype_skeleton, buildType, mContext.getString(R.string.bootleg_type_unofficial));
+
+            default:
+                return mContext.getString(R.string.bootleggers_releasetype_skeleton, buildType, mContext.getString(R.string.bootleg_type_unknown));
+        }
+    }
+}
+
