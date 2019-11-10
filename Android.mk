6f96a4169d0a1a5f82fb87297de19e161db399b1
SpiritCroc <dev@spiritcroc.de>
2019-11-10 20:56:45 +0100

    Build custom vendor support [1/2]
    Change-Id: I9348507521d71d2f5261e21f13c299f1e9144c3e

--- Android.mk
+++ Android.mk
@@ -35,7 +35,8 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.recyclerview_recyclerview \
     com.google.android.material_material \
     setupcompat \
-    setupdesign
+    setupdesign \
+    VendorSupport-preference
 
 LOCAL_JAVA_LIBRARIES := \
     telephony-common \
