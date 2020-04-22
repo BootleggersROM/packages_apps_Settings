AyanC1/packages_apps_Settings-1

Settings: Bring back Gaming mode [2/3]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: DennySPB <dennyspb@gmail.com>
Change-Id: I3bfb792366053d127ddbe9dc094a46c5525e3aae

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -150,6 +150,7 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.bootleggers.dumpster.MainDump;
+import com.bootleggers.dumpster.fragments.GamingMode;
 import com.bootleggers.dumpster.fragments.PowerMenuSettings;
 import com.bootleggers.dumpster.external.OmniJawsSettings;
 
@@ -293,6 +294,7 @@ public class SettingsGateway {
             PowerMenuSettings.class.getName(),
             RunningServices.class.getName(),
             OmniJawsSettings.class.getName()
+            GamingMode.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -335,5 +337,6 @@ public class SettingsGateway {
             Settings.PowerMenuSettings.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
             Settings.OmniJawsSettingsActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName(),
     };
 }

BootleggersROM/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -151,6 +151,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.bootleggers.dumpster.MainDump;
 import com.bootleggers.dumpster.fragments.BootlegWelcome;
+import com.bootleggers.dumpster.fragments.GamingMode;
 import com.bootleggers.dumpster.fragments.PowerMenuSettings;
 import com.bootleggers.dumpster.external.OmniJawsSettings;
 
@@ -294,7 +295,8 @@ public class SettingsGateway {
             PowerMenuSettings.class.getName(),
             BootlegWelcome.class.getName(),
             RunningServices.class.getName(),
-            OmniJawsSettings.class.getName()
+            OmniJawsSettings.class.getName(),
+            GamingMode.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -337,5 +339,6 @@ public class SettingsGateway {
             Settings.PowerMenuSettings.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
             Settings.OmniJawsSettingsActivity.class.getName(),
+            Settings.GamingModeActivity.class.getName(),
     };
 }